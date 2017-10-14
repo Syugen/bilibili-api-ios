8e65d6419b26fb9137904f0f864450e7cf1a23bf
Yuan Zhou <syugen.zhou@mail.utoronto.ca>
2017-10-14 16:22:01 -0400

    Initial Commit

---
+++ BilibiliAPI/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BilibiliAPI
+//
+//  Created by Yuan Zhou on 2017/10/14.
+//  Copyright © 2017年 Yuan Zhou. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
