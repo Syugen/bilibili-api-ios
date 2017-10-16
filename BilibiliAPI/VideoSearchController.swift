ca5f0ae5d855d0b8c459c39cc38dc302d7b9a36f
Yuan Zhou <syugen.zhou@mail.utoronto.ca>
2017-10-16 01:48:42 -0400

    simple http get request

--- BilibiliAPI/VideoSearchController.swift
+++ BilibiliAPI/VideoSearchController.swift
@@ -18,19 +18,42 @@ class VideoSearchController: UIViewController, UISearchBarDelegate {
 
         // Do any additional setup after loading the view.
         self.navigationItem.titleView = searchBar
-        searchBar.keyboardType = UIKeyboardType.numberPad
-        searchBar.becomeFirstResponder()
+        self.searchBar.keyboardType = UIKeyboardType.numberPad
+        self.searchBar.becomeFirstResponder()
         if #available(iOS 11.0, *) {
-            searchBar.heightAnchor.constraint(equalToConstant: 44).isActive = true
+            self.searchBar.heightAnchor.constraint(equalToConstant: 44).isActive = true
         }
         
         let searchButton = UIBarButtonItem(title: "Search", style: .plain, target: self, action: #selector(searchPressed))
         self.navigationItem.setRightBarButton(searchButton, animated: true)
     }
     
-    @objc func searchPressed(_ sender: Any) {
-        searchBar.resignFirstResponder()
-        content.text = "Searching " + searchBar.text!
+    @objc func searchPressed(_ sender: UIBarButtonItem) {
+        self.searchBar.resignFirstResponder()
+        self.content.text = "Searching " + searchBar.text!
+        let url = URL(string: "https://api.bilibili.com/archive_stat/stat?aid=" + self.searchBar.text!)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    
+                    // The following is just a playground
+                    if let json = String(data: data, encoding: .utf8) {
+                        print(json)
+                        DispatchQueue.main.async(execute: {
+                            self.content.text = json
+                        })
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
